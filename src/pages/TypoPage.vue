ebecbe66f078b51d57086a85721aaeb45054c4c7
joelgrod <joelgrod@gmail.com>
2021-09-22 13:58:25 +0100

    TypoPage

--- src/pages/TypoPage.vue
+++ src/pages/TypoPage.vue
@@ -1,6 +1,31 @@
 <template>
-  <q-page class="flex">
-    <h1>Typography</h1>
+  <q-page class="flex column q-pa-xs">
+    <span class="text-h1 text-center q-pa-xs">Headline 1</span>
+    <span class="text-h2 text-center q-pa-xs">Headline 2</span>
+    <span class="text-h3 text-center q-pa-xs">Headline 3</span>
+    <span class="text-h4 text-center q-pa-xs">Headline 4</span>
+    <span class="text-h5 text-center q-pa-xs">Headline 5</span>
+    <span class="text-h6 text-center q-pa-xs">Headline 6</span>
+
+    <q-separator />
+
+    <span class="text-subtitle1 text-center q-pa-xs">Subtitle 1</span>
+    <span class="text-subtitle2 text-center q-pa-xs">Subtitle 2</span>
+
+    <q-separator />
+    
+    <p class="text-body1 text-center q-pa-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed suscipit cumque, reiciendis molestias eum eius assumenda quasi aperiam rerum soluta quisquam labore consequuntur perspiciatis vitae sit harum eveniet eligendi aliquid?</p>
+    <p class="text-body2 text-center q-pa-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed suscipit cumque, reiciendis molestias eum eius assumenda quasi aperiam rerum soluta quisquam labore consequuntur perspiciatis vitae sit harum eveniet eligendi aliquid?</p>
+    
+    <q-separator />
+  
+    <span class="text-weight-thin text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    <span class="text-weight-light text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    <span class="text-weight-regular text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    <span class="text-weight-medium text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    <span class="text-weight-bold text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    <span class="text-weight-bolder text-center q-pa-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+
   </q-page>
 </template>
 
