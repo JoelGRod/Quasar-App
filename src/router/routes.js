82bec015a675e25028119d9e23ab49e3d03413f8
joelgrod <joelgrod@gmail.com>
2021-09-22 13:18:54 +0100

    first commit

---
+++ src/router/routes.js
@@ -0,0 +1,32 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { 
+        path: '', 
+        component: () => import('pages/Index.vue') 
+      },
+      { 
+        path: 'typography',
+        name: 'Typo',
+        component: () => import('pages/TypoPage.vue') 
+      },
+      { 
+        path: 'flex',
+        name: 'Flex',
+        component: () => import('pages/flex.vue') 
+      },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
